f260b8d72d2aa273c2c86429f13ae92f5104888a
unknown <nikolajoksimovic95@gmail.com>
2023-01-19 00:29:18 +0100

    finish home page

--- js/index.js
+++ js/index.js
@@ -5,4 +5,19 @@ $(window).ready(() => {
   $(".close-btn").click(() => {
     $("nav").removeClass("nav-show");
   });
+
+  // active link effect
+  $("#home").addClass("link-active");
+  $(".link").click((e) => {
+    $(".link").removeClass("link-active");
+    $(e.target).removeClass("link-hover").addClass("link-active");
+  });
+  // hover link effect
+  $(".link").hover((e) => {
+    if (!$(e.target).hasClass("link-active")) {
+      $(e.target).addClass("link-hover");
+    } else {
+      $(e.target).removeClass("link-hover");
+    }
+  });
 });
