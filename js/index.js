8610aa6621ff8a9dbe135f48cd06838bd5adec28
unknown <nikolajoksimovic95@gmail.com>
2023-01-18 17:21:35 +0100

    make tabled screen styling

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 $(window).ready(() => {
   $(".menu-btn").click(() => {
-    console.log("show");
     $("nav").addClass("nav-show");
   });
   $(".close-btn").click(() => {
-    console.log("hide");
     $("nav").removeClass("nav-show");
   });
 });
