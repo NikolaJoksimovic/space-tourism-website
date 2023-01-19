b705d3e2eee0606fb67b73b46ce0ca1a1f411aa5
unknown <nikolajoksimovic95@gmail.com>
2023-01-19 14:18:18 +0100

    finish destinations layout

--- js/destination.js
+++ js/destination.js
@@ -1,4 +1,7 @@
+var pathname = window.location.pathname;
+var page_id = pathname.slice(pathname.indexOf("-") + 1, pathname.indexOf("."));
 $(window).ready(() => {
   // active link effect
   $("#destination").addClass("link-active");
+  $(`#${page_id}`).addClass("destination-link-active");
 });
